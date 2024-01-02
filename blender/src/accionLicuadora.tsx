bae48e34c25e9227e524bc0f21af2ec79d15b96d
Blanc Luis Maximiliano <machys1985@gmail.com>
2024-01-02 10:57:57 -0300

    no se me ve la foto

--- blender/src/accionLicuadora.tsx
+++ blender/src/accionLicuadora.tsx
@@ -1,4 +1,27 @@
 import { useState } from "react";
+import sound from './blender-mixer-smoothie-33026.mp3';
 
-let presionar = false
+const Licuadora = () => {
+    
+    const[presionar, setPresionar] = useState(false)
+    
+    const handleClick =() =>{
+        if (setPresionar === true)
+            sound.play();
+    }  
+    
+    return(
+        <div
+        id="licuadora"
+        className={presionar?'shake':''}
+        onClick={handleClick}
+        >
+    <h2>Mi Licuadora</h2>
+    <img src="./blender-svgrepo-com.svg"/>
+    <p>Haz click en el boton para accionar la Licuadora</p>    
+    </div>
+    );
+};
 
+export default Licuadora;
+    

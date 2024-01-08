cd72d7ac8b6696a1b44bfedc6a74de5306d3eae5
Blanc Luis Maximiliano <machys1985@gmail.com>
2024-01-08 14:17:28 -0300

    agregando Cards

--- dark-mode/dark-mode/src/cards/Cards.tsx
+++ dark-mode/dark-mode/src/cards/Cards.tsx
@@ -1,11 +1,12 @@
 import { Button } from './Button';
 import { CardStyle } from './cardStyle';
+import cardContent from './cardContent';
 
 const cards = () => {
     return (
         <div>
-            <CardStyle>aca va la tarjeta</CardStyle>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam reiciendis at totam nulla eveniet tenetur.</p>
+            <CardStyle><cardContent/></CardStyle>
+            
             <Button>More</Button>
         </div>
     );
---
+++ dark-mode/dark-mode/src/cards/cardContent.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+const CardContainer = styled.div`
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
+  margin: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const CardImage = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+`;
+
+const CardBody = styled.div`
+  padding: 16px;
+`;
+
+const CardTitle = styled.h5`
+  margin-bottom: 8px;
+`;
+
+const CardDescription = styled.p`
+  color: #555;
+`;
+
+const cardContent = () => {
+    const cards = [
+        {
+            title: 'Card 1',
+            imageSrc: './AMD_R7260X.jpg',
+            description: '',
+        },
+        {
+            title: 'Card 1',
+            imageSrc: './AMD_9280X.jpg',
+            description: '',
+        },
+        {
+            title: 'Card 1',
+            imageSrc: './AMD_RX550.jpg',
+            description: '',
+        },
+    ];
+    return (
+        <div>
+            {cards.map((card, index) => (
+        <CardContainer key={index}>
+          <CardImage src={card.imageSrc} alt={`Card ${index + 1}`} />
+          <CardBody>
+            <CardTitle>{card.title}</CardTitle>
+            <CardDescription>{card.description}</CardDescription>
+          </CardBody>
+        </CardContainer>
+      ))}
+        </div>
+    );
+};
+
+export default cardContent;
